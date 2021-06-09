0fc01d447d7e14568e6dec61422fb76b63d4b8cc
sarthak503 <sarthak.kumar503@gmail.com>
2021-06-09 22:57:11 +0530

    Commmit with no mobile respo

---
+++ index.js
@@ -0,0 +1,14 @@
+var CountDownDate = new Date("June 26, 2021 09:00:00").getTime();
+var x =setInterval(function(){
+    var now = new Date().getTime();
+    var dist = CountDownDate - now;
+    var days = Math.floor(dist / (1000 * 60*60 * 24));
+    var hours = Math.floor((dist %(1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    var mins = Math.floor((dist %(1000 * 60 * 60)) / (1000 * 60));
+    var sec = Math.floor((dist %(1000 * 60)) / (1000));
+
+document.getElementById("day").innerHTML = days;
+document.getElementById("hour").innerHTML = hours;
+document.getElementById("minute").innerHTML = mins;
+document.getElementById("second").innerHTML = sec;
+})
